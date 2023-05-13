ee431d0ac7211b50c9ae1e708d0b14d72c72d82d
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-13 14:12:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿﻿namespace test
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            string singZodiac;
+            int age;
+            Console.Write("Ввудите ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Какой ваш знак зодиака: ");
+            singZodiac = Console.ReadLine();
+            Console.Write("Сколько вам полных лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            string profession;
+            Console.Write("Кем вы работаете:");
+            profession = Console.ReadLine();
+            Console.WriteLine($"Здравствуйте, вас зовут {name}, вам {age} год, вы {singZodiac}, ваша професия {profession}!");
+        }
+    }
+}
