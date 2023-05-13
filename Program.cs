844b60948b84c9c0574655fa0422d8dd94ac0cf9
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-13 15:18:50 +0300

    init

--- Program.cs
+++ Program.cs
@@ -4,19 +4,15 @@
     {
         static void Main()
         {
-            string name;
-            string singZodiac;
-            int age;
-            Console.Write("Ввудите ваше имя: ");
-            name = Console.ReadLine();
-            Console.Write("Какой ваш знак зодиака: ");
-            singZodiac = Console.ReadLine();
-            Console.Write("Сколько вам полных лет: ");
-            age = Convert.ToInt32(Console.ReadLine());
-            string profession;
-            Console.Write("Кем вы работаете:");
-            profession = Console.ReadLine();
-            Console.WriteLine($"Здравствуйте, вас зовут {name}, вам {age} год, вы {singZodiac}, ваша професия {profession}!");
+            string name = "Evtushenko";
+            string famileName = "Valentin";
+            Console.WriteLine(name + " " + famileName);
+            string trueName;
+            string trueFamilyName;
+            trueName = famileName;
+            trueFamilyName = name;
+            Console.WriteLine(trueName + " " + trueFamilyName);
+         
         }
     }
 }
