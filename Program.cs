bdf1f376cf4ec17bc876995c8983c38a5a330f25
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-14 14:30:35 +0300

    init

--- Program.cs
+++ Program.cs
@@ -4,15 +4,19 @@
     {
         static void Main()
         {
-            string name = "Evtushenko";
-            string famileName = "Valentin";
-            Console.WriteLine(name + " " + famileName);
-            string trueName;
-            string trueFamilyName;
-            trueName = famileName;
-            trueFamilyName = name;
-            Console.WriteLine(trueName + " " + trueFamilyName);
-         
+            int gold;
+            int crystals;
+            int pricePerCrystal = 8;
+            bool abilityToPay;
+            Console.WriteLine($"добро пожаловать в магазин, сегодня стоимость кристалов составляет {pricePerCrystal} золота.");
+            Console.Write("сколько у вас золота? ");
+            gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("сколько вы хотите купить кристалов?");
+            crystals = Convert.ToInt32(Console.ReadLine());
+            abilityToPay = gold >= crystals * pricePerCrystal;
+            crystals *= (Convert.ToInt32(abilityToPay));
+            gold -= crystals * pricePerCrystal;
+            Console.WriteLine($"У вас осталось {gold} золота, у вас в наличии {crystals} кристалов");
         }
     }
 }
