a94f2e7191a9320066452eec12f82d33839a1e3f
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-05-14 14:51:28 +0300

    init

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@
             int gold;
             int crystals;
             int pricePerCrystal = 8;
-            bool abilityToPay;
+            bool canPay;
             Console.WriteLine($"добро пожаловать в магазин, сегодня стоимость кристалов составляет {pricePerCrystal} золота.");
             Console.Write("сколько у вас золота? ");
             gold = Convert.ToInt32(Console.ReadLine());
             Console.Write("сколько вы хотите купить кристалов?");
             crystals = Convert.ToInt32(Console.ReadLine());
-            abilityToPay = gold >= crystals * pricePerCrystal;
-            crystals *= (Convert.ToInt32(abilityToPay));
+            canPay = gold >= crystals * pricePerCrystal;
+            crystals *= (Convert.ToInt32(canPay));
             gold -= crystals * pricePerCrystal;
             Console.WriteLine($"У вас осталось {gold} золота, у вас в наличии {crystals} кристалов");
         }
