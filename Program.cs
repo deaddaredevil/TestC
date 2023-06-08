6f267f97a1fb89591d7337ff7d2d2216c1abdd62
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-06-08 13:02:22 +0300

    init

--- Program.cs
+++ Program.cs
@@ -6,15 +6,14 @@ namespace test
     {
         static void Main()
         {
-            int startCycle = 5;
-            int endCycle = 100;
+            int startNumber = 5;
+            int endCycle = 96;
             int stepCycle = 7;
 
-            for (int i = startCycle; i < endCycle; i += stepCycle)
+            for (int i = startNumber; i <= endCycle; i += stepCycle)
             {
                 Console.Write(i + " ");
             } 
-            //Данный цикл выбран потому что мы знаем конечную точку и шаг, в отличии от while где неизвестно колличество цикла.
         }
     }
 }
