bab35fb89298dcd5cbab3b2c21e7782aa89fd2d4
deaddaredevil <113775017+deaddaredevil@users.noreply.github.com>
2023-06-10 02:42:10 +0300

    init

--- Program.cs
+++ Program.cs
@@ -9,18 +9,26 @@ namespace test
             Random random = new Random();
             int minNumber = 0;
             int maxNumber = 100;
-            int number = random.Next(minNumber, maxNumber +1);
-            int startNumber = number;
-            int divisorNumber = 3;
+            int number = random.Next(minNumber, maxNumber + 1);
+            int divisorNumber1 = 3;
+            int divisorNumber2 = 5;
+            int sumNumbers = number;
 
-            Console.WriteLine(number);
+            Console.WriteLine("начальное число " + number);
 
-            for (int i = number; startNumber > 0; i = divisorNumber) 
+            for (int i = 0; i < number; ++i)
             {
-                number += startNumber - divisorNumber;
-                startNumber-=divisorNumber;        
-                Console.WriteLine(number);
+                if (i % divisorNumber1 == 0 || i % divisorNumber2 == 0)
+                {
+                    sumNumbers += i;
+                    Console.WriteLine(sumNumbers);
+                }
+                else
+                {
+                    continue;
+                }
             }
+
         }
     }
 }
